e8b48b9bb75f997e23aff2cadc3a4b8020ae181c
pasDamola <oyindadavid18@gmail.com>
2024-02-07 07:26:49 +0100

    product data model

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Product struct {
+	Name          string    `json:"name"`
+	Description   string  `json:"description"`
+	Status        string  `json:"status"`
+	CreatedAt  time.Time `json:"createdAt"`
+	OnlineDate  time.Time `json:"onlineDate"`
+}
+
+
 func main() {
  router := gin.Default()
  router.Run(":9003")
