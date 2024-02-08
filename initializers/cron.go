0b9f41e5b235308af918763c4673eda8770a422d
pasDamola <oyindadavid18@gmail.com>
2024-02-08 14:43:52 +0100

    a consumer function that reads product data from the rabbitmq

--- initializers/cron.go
+++ initializers/cron.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/pasDamola/feature-case-study/models"
 	"github.com/robfig/cron/v3"
@@ -16,9 +17,10 @@ var c *cron.Cron
 func StartProductCron() {
 	 // Set up cron job
 	 c = cron.New()
+	 
 
-	 // Schedule the cron job to run every day at 2:00 AM
-	 c.AddFunc("* * * * *", processNewRecords)
+	 // Schedule the cron job to run every minute
+	 c.AddFunc("* * * * *", publishNewProducts)
  
 	 c.Start()
 }
@@ -28,23 +30,24 @@ func queryNewProducts() []models.CatalogProduct {
     result := DB.Where("status = ?", "new").Find(&products)
 
 	if result.Error != nil {
-        panic("failed to retrieve products with new status")
+        log.Println("failed to retrieve products with new status")
     }
 
 	return products
 }
 
-func processNewRecords() {
+func publishNewProducts() {
 	for _, product := range queryNewProducts() {
 		fmt.Println("product", product)
         publishToRabbitMQ(product)
         markAsInProgress(product)
     }
+
+	consumeNewProducts()
 }
 
 func markAsInProgress(product models.CatalogProduct) {
 	result := DB.Model(&models.CatalogProduct{}).Where("product_id = ? AND status = ?", product.ProductID, "new").Update("status", "in-progress")
-	fmt.Println(result.Error)
 	if result.Error != nil {
         log.Fatal("failed to update status")
     }
@@ -52,7 +55,67 @@ func markAsInProgress(product models.CatalogProduct) {
 
 func publishToRabbitMQ(product models.CatalogProduct) {
     // Connect to RabbitMQ
-    conn, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
+    if err != nil {
+        fmt.Println("Failed to connect to RabbitMQ:", err)
+        return
+    }
+    defer conn.Close()
+
+    ch, err := conn.Channel()
+    if err != nil {
+        fmt.Println("Failed to open channel:", err)
+        return
+    }
+    defer ch.Close()
+
+    
+    q, err := ch.QueueDeclare(
+        os.Getenv("RABBITMQ_QUEUE"),
+        true,                        
+        false,                        
+        false,                        
+        false,                        
+        nil,                          
+    )
+    if err != nil {
+        
+        fmt.Println("Failed to declare queue:", err)
+        return
+    }
+    
+
+    
+    data, err := json.Marshal(product)
+    if err != nil {
+        // Handle error
+        fmt.Println("Failed to serialize record to JSON:", err)
+        return
+    }
+
+    
+    err = ch.Publish(
+        "", 
+        q.Name,
+        false,
+        false,
+        amqp.Publishing{
+            ContentType: "application/json",
+            Body:        data,
+        },
+    )
+    if err != nil {
+        
+        fmt.Println("Failed to publish message to RabbitMQ:", err)
+        return
+    }
+
+    fmt.Println("Record pushed to RabbitMQ")
+}
+
+
+func consumeNewProducts() {
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
     if err != nil {
         fmt.Println("Failed to connect to RabbitMQ:", err)
         return
@@ -81,30 +144,49 @@ func publishToRabbitMQ(product models.CatalogProduct) {
         return
     }
 
-    
-    data, err := json.Marshal(product)
-    if err != nil {
-        // Handle error
-        fmt.Println("Failed to serialize record to JSON:", err)
-        return
-    }
-
-    
-    err = ch.Publish(
-        "", 
+    // Consume messages from the queue
+    msgs, err := ch.Consume(
         q.Name,
+        "",
+        true,
         false,
         false,
-        amqp.Publishing{
-            ContentType: "application/json",
-            Body:        data,
-        },
+        false,
+        nil,
     )
     if err != nil {
-        
-        fmt.Println("Failed to publish message to RabbitMQ:", err)
-        return
+        log.Fatalf("Failed to consume messages: %s", err)
     }
 
-    fmt.Println("Record pushed to RabbitMQ")
+    // Process messages
+    for msg := range msgs {
+        // Simulate processing time
+        time.Sleep(1 * time.Second)
+
+        // Decode message into Product
+        var product models.CatalogProduct
+        err := json.Unmarshal(msg.Body, &product)
+        if err != nil {
+            log.Printf("Failed to decode message: %s", err)
+            continue
+        }
+
+		currentTime := time.Now()
+        currentTimeString := currentTime.Format("2006-01-02 15:04:05")
+
+        // Update online date column
+        product.OnlineDate = &currentTimeString
+
+        // Mark status as processed
+        product.Status = "processed"
+
+        // Update record in the database
+        err = DB.Save(&product).Error
+        if err != nil {
+            log.Printf("Failed to update record in database: %s", err)
+            continue
+        }
+
+        log.Printf("Processed product with ID %d", product.ProductID)
+    }
 }
